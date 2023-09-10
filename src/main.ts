7d7682743e92fe16e45c65ab92e90c1aa31cb46b
mamasitta <vikiber2013@gmail.com>
2023-09-10 13:17:56 +0200

    fixing TS

--- src/main.ts
+++ src/main.ts
@@ -15,31 +15,31 @@ WA.onInit().then(() => {
 
  // Julia custom
 
-    // At 19:00, turn on night
-    const cronStartNight = parseCronExpression('0 19 * * *');
-    scheduler.setInterval(cronStartNight, () => {
-        WA.room.showLayer("night");
-        WA.room.showLayer("light");
-    });
+// At 19:00, turn on night
+const cronStartNight = parseCronExpression('0 19 * * *');
+scheduler.setInterval(cronStartNight, () => {
+    WA.room.showLayer("night");
+    WA.room.showLayer("light");
+});
 
-    // At 7:00, turn on day
-    const cronStartDay = parseCronExpression('0 7 * * *');
-    scheduler.setInterval(cronStartDay, () => {
-        WA.room.hideLayer("night");
-        WA.room.hideLayer("light");
-    });
+// At 7:00, turn on day
+const cronStartDay = parseCronExpression('0 7 * * *');
+scheduler.setInterval(cronStartDay, () => {
+    WA.room.hideLayer("night");
+    WA.room.hideLayer("light");
+});
 
-    // If the player enters the room between 19:00 and 7:00, turn on night
-    const date = new Date();
-    const startNight = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 19, 0, 0);
-    const startDay = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 7, 0, 0);
-    if (date > startNight || date < startDay) {
-        console.log(startDay)
-        console.log(startNight)
-        console.log(date)
-        WA.room.showLayer("night");
-        WA.room.showLayer("light");
-    }
+// If the player enters the room between 19:00 and 7:00, turn on night
+const date = new Date();
+const startNight = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 19, 0, 0);
+const startDay = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 7, 0, 0);
+if (date < startNight || date > startDay) {
+    console.log(startDay)
+    console.log(startNight)
+    console.log(date)
+    WA.room.hideLayer("night");
+    WA.room.hideLayer("light");
+}
 
     WA.room.onEnterLayer("floor").subscribe(() => {
         WA.room.hideLayer("roof");
