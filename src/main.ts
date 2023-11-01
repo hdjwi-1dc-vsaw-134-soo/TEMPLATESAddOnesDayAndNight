9966eb12b2047faef4cb16dae4ca8e13481c8f45
mamasitta <vikiber2013@gmail.com>
2023-11-01 11:40:27 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
 import {parseCronExpression} from "cron-schedule";
 import {TimerBasedCronScheduler as scheduler} from "cron-schedule/schedulers/timer-based.js";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
+
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
